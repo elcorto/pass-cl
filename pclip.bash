036c3b22b4c3b202e835d4b1b8b1d8923e99365d
Steve Schmerler <git@elcorto.com>
2018-09-16 00:20:11 +0200

    ENH: handle nonexistent "xclip -r" flag, add verbose error msg

--- pclip.bash
+++ pclip.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+err(){
+    die "pass pclip: $@"
+}
+
+do_xclip(){
+    # Some versions of xclip seem to have a -r flag. Ours doesn't (Debian).
+    stdin=$(cat)
+    echo "$stdin" | xclip -selection primary > /dev/null 2>&1
+    [ $? -eq 0 ] || echo "$stdin" | xclip -r > /dev/null 2>&1
+    [ $? -eq 0 ] || err "xclip failed"
+    echo "Copied metadata to primary selection."
+}
+
 local path="$1"
 local passfile="$PREFIX/$path.gpg"
 local key="${PASS_PCLIP_KEY}"
@@ -8,14 +21,17 @@ check_sneaky_paths "$path"
 
 if [[ -f $passfile ]]; then
     if [[ -z "$key" ]]; then
-        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 | head -n1 | xclip -r || exit $?
+        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 | head -n1 \
+            | do_xclip || err "could not copy metadata, exit $?"
     else
-        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 | grep -E -m1 "$key" | sed -E "s/$key//" | xclip -r || exit $?
+        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 \
+            | grep -E -m1 "$key" | sed -E "s/$key//" | do_xclip \
+            || err "could not copy metadata, exit $?"
     fi
 elif [[ -z $path ]]; then
     die ""
 else
-    die "Error: $path is not in the password store."
+    err "Error: $path is not in the password store."
 fi
 
 cmd_show --clip "$@"
